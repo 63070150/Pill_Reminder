f8bbb8902fbcfab693dbce67f35a59ad85af9b61
SarunC4 <74345299+SarunC4@users.noreply.github.com>
2022-11-21 01:06:12 +0700

    Add files via upload

--- screens/MedicationsScreen.js
+++ screens/MedicationsScreen.js
@@ -1,19 +1,45 @@
-import React from "react";
+import { collection, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { FlatList, ScrollView } from "react-native-gesture-handler";
+import db from "../database/firebaseDB";
 
 const MedicationsScreen = ({ navigation }) => {
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "Medicine"), getData, (error) => {
+      console.log(error);
+    });
+  }, []);
+  
+  function getData(querySnapshot) {
+    let dataFromFirebase = []
+    querySnapshot.forEach((res) => {
+      dataFromFirebase.push(res.data());
+    })
+    
+    setListData(dataFromFirebase)
+  }
+
+  function generateMedicine(value, index) {
+    return(
+      <View key={index} style={styles.medic}>
+        <Text>{value.name}</Text>
+        <Text style={{opacity: 0.4}}>{value.dosage} tabs | {value.number} times</Text>
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>MedicationsScreen</Text>
-      <Button
-        title="Click here"
-        onPress={() => alert("Button Clicked")}
-      ></Button>
-    </View>
+
+      <ScrollView contentContainerStyle={{paddingBottom: "85%", paddingTop: 20, width: "100%", height: "100%", alignItems: "center"}}>
+      {listData.map(generateMedicine)}
+      </ScrollView>
+
   );
 };
 
-export default MedicationsScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -22,4 +48,20 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "#fff",
   },
+  medic: {
+    width: "90%",
+    height: "25%",
+    paddingLeft: 20,
+    paddingRight: 20,
+    justifyContent: "center",
+    marginBottom: 20,
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 5,
+    borderRadius: 5,
+    backgroundColor: "#fff"
+  },
+
 });
+
+export default MedicationsScreen;
