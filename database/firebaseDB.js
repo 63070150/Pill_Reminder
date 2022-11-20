1cab85c2ff77b143695e7fc81d5c4bf2847276ad
SarunC4 <74345299+SarunC4@users.noreply.github.com>
2022-11-20 23:08:31 +0700

    Add files via upload

---
+++ database/firebaseDB.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/compat";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAXNr3qAri4xTLMUHu0-A7h0CfGH9f1gic",
+    authDomain: "pill-reminder-e5475.firebaseapp.com",
+    projectId: "pill-reminder-e5475",
+    storageBucket: "pill-reminder-e5475.appspot.com",
+    messagingSenderId: "10735431858",
+    appId: "1:10735431858:web:fae427d98d1d19352d19ab",
+    measurementId: "G-DN5LD3MR5C"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
